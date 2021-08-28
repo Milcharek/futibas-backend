00853c14a24c03844ad846d3850630f6ffcd45a4
Filipe Milcharek <ofilipedavila@gmail.com>
2021-08-28 19:12:24 -0300

    Add CreateAppointmentService

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
-import { startOfHour, parseISO } from 'date-fns';
+import { parseISO } from 'date-fns';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
@@ -13,22 +14,23 @@ appointmentsRouter.get('/', (request, response) => {
 })
 
 appointmentsRouter.post('/', (request, response) => {
-    const { field, date } = request.body;
+    try {
+        const { field, date } = request.body;
 
-    const parsedDate = startOfHour(parseISO(date));
-
-    const findAppointmentInSameDate = appointmentsRepository.findByDate(parsedDate);
-
-    if(findAppointmentInSameDate) {
-        return response.status(400).json({ message: "This appointment is already booked!" })
+        const parsedDate = parseISO(date);
+        
+        const createAppointment = new CreateAppointmentService(appointmentsRepository);
+    
+        const appointment = createAppointment.execute({ 
+            date: parsedDate, 
+            field 
+        });
+    
+        return response.json(appointment);
+    } 
+    catch (err) {
+        return response.status(400).json({ error: err.message });
     }
-
-    const appointment = appointmentsRepository.create({
-        field, 
-        date: parsedDate
-    });
-
-    return response.json(appointment);
 })
 
 export default appointmentsRouter;
---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,36 @@
+import { startOfHour } from "date-fns";
+
+import Appointment from "../models/Appointment";
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+interface IRequest {
+    field: string;
+    date: Date;
+}
+
+class CreateAppointmentService {
+    private appointmentsRepository: AppointmentsRepository;
+
+    constructor(appointmentsRepository: AppointmentsRepository) {
+        this.appointmentsRepository = appointmentsRepository;
+    }
+
+    public execute({ field, date }: IRequest): Appointment {
+        const appointmentDate = startOfHour(date);
+
+        const findAppointmentInSameDate = this.appointmentsRepository.findByDate(appointmentDate);
+    
+        if(findAppointmentInSameDate) {
+            throw Error('This appointment is already booked!');
+        }
+    
+        const appointment = this.appointmentsRepository.create({
+            field, 
+            date: appointmentDate
+        });
+
+        return appointment;
+    }
+}
+
+export default CreateAppointmentService;
