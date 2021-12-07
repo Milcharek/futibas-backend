1ce6f842b91c7d4b4704702b9426c7b8a3e0d059
Filipe Milcharek <ofilipedavila@gmail.com>
2021-12-07 09:07:35 -0300

    Refactor Appointments Module

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -5,14 +5,14 @@ import User from '../infra/typeorm/entities/User';
 
 import AppError from '@shared/errors/AppError';
 
-interface iCreateUserRequest {
+interface ICreateUserRequest {
     name: string,
     email: string,
     password: string
 }
 
 class CreateUserService {
-    async execute({ name, email, password }: iCreateUserRequest): Promise<User> {
+    async execute({ name, email, password }: ICreateUserRequest): Promise<User> {
         const usersRepository = getRepository(User);
 
         const checkUserExists = await usersRepository.findOne({
